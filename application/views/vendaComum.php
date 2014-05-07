3399bc79a639122d3151d4382948335134c3cbf0
unknown <Kaue@.(none)>
2014-05-06 22:51:45 -0400

    Ajustes

--- application/views/vendaComum.php
+++ application/views/vendaComum.php
@@ -1,12 +1,12 @@
 <script language="JavaScript">$('.dropdown-toggle').dropdown();</script>
 <div class="panel panel-default">
 	<div class="panel-body">
-		<div class="row">
-			<div class="panel panel-default">
-				<div class="container-fluid">
-					<br />
-					<form class="form-horizontal" method="post" role="form">
-						<div class="col-md-6 col-md-offset-3">
+		<div class="panel panel-default">
+			<div class="container-fluid">
+				<br />
+				<form class="form-horizontal" method="post" role="form">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3" align="center">
 							<div class="form-group">
 								<label for="inputEmail3" class="col-sm-5 control-label">Nome do Cliente</label>
 								<div class="col-sm-7">
@@ -14,9 +14,32 @@
 								</div>
 							</div>
 						</div>
-						<div class="col-md-6 col-md-offset-3">
+					</div>
+					<div class="row">
+						<div class="col-xs-12 col-md-8">
+							<table class="table table-bordered table-hover" style="margin-left: 15%">
+								<thead>
+									<th>Código do Poroduto</th>
+									<th>Quantidade</th>
+									<th>Valor</th>
+									<th> Deletar </th>
+								</thead>
+								<tbody>
+
+								</tbody>
+								<tfoot>
+									<td > Total </td>
+								</tfoot>
+							</table>
 						</div>
-						<div class="col-md-6 col-md-offset-3">
+						<div class="col-xs-6 col-md-4" align="center">
+							<button type="button" class="btn btn-primary" onclick="">
+								Adicionar Produto
+							</button>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3" align="center">
 							<ul class="pager">
 								<li>
 									<a href="<?php echo site_url("home")?>">Sair</a>
@@ -26,8 +49,8 @@
 								</li>
 							</ul>
 						</div>
-					</form>
-				</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
