a4fafead4ffb48b9c2a6be0c1975b3fecbe3c4c2
unknown <Michel@.(none)>
2014-06-04 13:47:03 -0400

    Ajuste na view infoCliente

--- application/views/infoCliente.php
+++ application/views/infoCliente.php
@@ -11,57 +11,59 @@
 								<h2><?php echo $cliente[0]["nome_cliente"]; ?></h2>
 							</div>
 							
-							<div class="form-line panel panel-default" style="width: 100%; padding:10px">
-								<div style="float: left; margin-right: 3%; width: 47%">
-									<div class="subtitulo-info-cliente" align="center">
-										Dados Pessoais
-									</div>	
-									<div class="info-cliente-padding">
-										<div class="form-spacing-label" style="background-color: #CCCCCC;">
+							<div class="form-line" style="width: 100%;">
+								<div style="float: left; margin-right: 1%; width: 49%">																	
+									<div class="info-cliente">
+										<div class="subtitulo-info-cliente" align="center">
+											Dados Pessoais
+										</div>	
+										<div class="form-spacing-label" style="background-color: #EEEEEE;">
 											<label>Contato: </label>
 											<span><?php echo $cliente[0]["tel_cliente"]; ?></span>
 										</div>
-										<div class="form-spacing-label" style="background-color: #EEEEEE;">
+										<div class="form-spacing-label">
 											<label>CPF: </label>
 											<span><?php echo $cliente[0]["cpf_cliente"]; ?></span>
 										</div>	
-										<div class="form-spacing-label" style="background-color: #CCCCCC;">
+										<div class="form-spacing-label" style="background-color: #EEEEEE;">
 											<label>Referência Comercial: </label>
 											<span><?php echo $cliente[0]["ref_comercial"]; ?></span>
 										</div>	
 									</div>																						
 								</div>	
 								<div style="float: left; width: 50%">
-									<div class="subtitulo-info-cliente" align="center">
-										Endereço
-									</div>	
-									<div class="info-cliente-padding">
-										<div class="form-spacing-label" style="background-color: #CCCCCC;"	>
+									
+									<div class="info-cliente">
+										<div class="subtitulo-info-cliente" align="center">
+											Endereço
+										</div>	
+										<div class="form-spacing-label" style="background-color: #EEEEEE;"	>
 											<label>Rua: </label>
 											<span><?php echo $cliente[0]["rua_endereco"]; ?></span>
 										</div>
-										<div class="form-spacing-label" style="background-color: #EEEEEE;">
+										<div class="form-spacing-label">
 											<label>Número: </label>
 											<span><?php echo $cliente[0]["numero_endereco"]; ?></span>
 										</div>	
-										<div class="form-spacing-label" style="background-color: #CCCCCC;">
+										<div class="form-spacing-label" style="background-color: #EEEEEE;">
 											<label>Complemento: </label>
 											<span><?php echo $cliente[0]["rua_endereco"]; ?></span>
 										</div>		
-										<div class="form-spacing-label" style="background-color: #EEEEEE;">
+										<div class="form-spacing-label">
 											<label>Bairro: </label>
 											<span><?php echo $cliente[0]["bairro_endereco"]; ?></span>
 										</div>	
-										<div class="form-spacing-label" style="background-color: #CCCCCC;">
+										<div class="form-spacing-label" style="background-color: #EEEEEE;">
 											<label>Cidade - UF: </label>
 											<span><?php echo $cliente[0]["cidade_endereco"] . ' - ' . $cliente[0]["uf_endereco"]; ?></span>
 										</div>	
 									</div>																				
 								</div>							
 							</div>	
-							<div class="info-cliente-padding" align="center">
+							<div style="padding: 10px;" align="center">
 								<input type="button" class="btn btn-primary" value="Editar" />
 								<input type="button" class="btn btn-default" value="Compras do Cliente" />
+								<input type="button" class="btn btn-default" value="Excluir Cliente" />
 							</div>						
 						</div>
 					</div>
